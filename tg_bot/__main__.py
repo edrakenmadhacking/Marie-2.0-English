04dcf5f36713000303f7478b80982147a4d43924
edrakenmadhacking <59361969+edrakenmadhacking@users.noreply.github.com>
2020-06-24 23:07:37 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -20,11 +20,13 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 PM_START_TEXT = """
 hoi {}, my name is {}!
 
-im a group manager bot maintained by  [this wonderfull person](tg://user?id=989362465).
+im a group manager bot maintained by [this wonderfull person](tg://user?id=989362465).
+
+Thanks to [this person](tg://user?id=686021814) for contribution.
 
 For more commands or questions on how to use me click /help...
 
-Want to add me on your group too?? [Click Here](http://telegram.me/Lbrittobot?startgroup=botstart)
+Want to add me on your group too?? [Click Here](http://telegram.me/froggyrobot?startgroup=botstart)
 
 """
 
